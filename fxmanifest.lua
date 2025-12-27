115671e79e2c26824554bdf07790a6524cd7ebff
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-27 22:34:18 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Routier87'
+description 'Zoo Normandie - Parc animalier RP désertique'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
