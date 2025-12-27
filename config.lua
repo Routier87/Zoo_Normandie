dd7d481e74873e05e3d2db43fec1787fcbc9f542
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-27 22:34:45 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.ZooName = "Zoo Normandie"
+
+Config.ZooZone = {
+    center = vector3(1660.0, 3150.0, 40.0),
+    radius = 250.0
+}
+
+Config.Animals = {
+    -- Savane
+    {model = 'a_c_elephant', coords = vector3(1662.3, 3155.1, 40.5), heading = 90.0},
+    {model = 'a_c_giraffe', coords = vector3(1668.7, 3160.2, 40.6), heading = 180.0},
+    {model = 'a_c_zebra', coords = vector3(1675.0, 3165.0, 40.4), heading = 270.0},
+
+    -- Félins
+    {model = 'a_c_tiger', coords = vector3(1700.5, 3120.8, 41.0), heading = 45.0},
+    {model = 'a_c_mtlion', coords = vector3(1705.2, 3125.3, 41.0), heading = 135.0},
+
+    -- Singes
+    {model = 'a_c_chimp', coords = vector3(1680.0, 3180.0, 45.0), heading = 0.0}
+}
